a070d782175c51b0311757579f95e471e448a9eb
cassandrabarbe <cassandra.barbe@gmail.com>
2016-03-18 16:16:49 -0400

    js done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-${'html').on('keydown'), function (e) {
-  var $newDiv - $('div');
-  $newDiv.addClass('ball');
-}
+var $html = ('html');
+var $body = $('body');
 
-var$body =
+$html.on('keydown', function (e) {
+  var div = $('<div>');
+
+  $body.append(div);
+  div.addClass('ball');
+  div.css('top', Math.random() * 1000);
+  div.css('left', Math.random() * 1000);
+});
